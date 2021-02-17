cd2476bb0e6478094dd3a6fe6ef34ce7b071adcf
Baran Önen <baranonenn@gmail.com>
2021-02-17 11:07:03 +0300

    Initial Commit

---
+++ AboutThisMac/AboutThisMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutThisMacApp.swift
+//  AboutThisMac
+//
+//  Created by Baran Önen on 17.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AboutThisMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutThisMacTests/AboutThisMacTests.swift
@@ -0,0 +1,33 @@
+//
+//  AboutThisMacTests.swift
+//  AboutThisMacTests
+//
+//  Created by Baran Önen on 17.02.2021.
+//
+
+import XCTest
+@testable import AboutThisMac
+
+class AboutThisMacTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
